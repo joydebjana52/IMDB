6f2c8e3f2a695a98dc4f7a299d5c38ef215e01dc
Joydeb Jana <janajoydeb083@gmail.com>
2025-11-26 23:12:32 +0530

    Update useEffect dependencies and cleanup data fetch to remove console warnings

--- src/components/movieList/movieList.jsx
+++ src/components/movieList/movieList.jsx
@@ -9,18 +9,10 @@ const MovieList = () => {
     const {type} = useParams()
 
     useEffect(() => {
-        getData()
-    }, [])
-
-    useEffect(() => {
-        getData()
-    }, [type])
-
-    const getData = () => {
         fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
-        .then(res => res.json())
-        .then(data => setMovieList(data.results))
-    }
+            .then(res => res.json())
+            .then(data => setMovieList(data.results))
+        }, [type])
 
     return (
         <div className="movie__list">
